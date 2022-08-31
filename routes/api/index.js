d9a93684e2fabcda11c227f46f8b0fb4c702dc5c
ThereWolf <bdunham484@gmail.com>
2022-08-31 11:57:30 -0500

    add thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,13 @@
 const router = require('express').Router();
 //import user-routes
 const userRoutes = require('./user-routes');
+//import thought-routes
+const thoughtRoutes = require('./thought-routes');
 
-//add prefix of '/user' to routes created in 'user-routes.js'
+//add prefix of '/users' to routes created in 'user-routes.js'
 router.use('/users', userRoutes);
+//add prefix of '/thoughts' to routes created in 'thought-routes.js'
+router.use('/thoughts', thoughtRoutes)
 
 //export routes
 module.exports = router;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+//import express.js router
+const router = require('express').Router();
+//import thought controllers
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller');
+
+router  
+    .route('/')
+    .get(getAllThoughts);
+
+router
+    .route('/:id')
+    .put(updateThought)
+    .get(getThoughtById);
+
+router
+    .route('/:userId')
+    .post(addThought);
+    
+
+router
+    .route('/:userId/:thoughtId')
+    .delete(deleteThought);
+
+//export routes
+module.exports = router;
