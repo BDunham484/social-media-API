e664d9d067e15f981c14bff4a8d2682ed7c9e872
ThereWolf <bdunham484@gmail.com>
2022-08-31 07:03:49 -0500

    add db connection

---
+++ server.js
@@ -0,0 +1,26 @@
+//import the express.js package
+const express = require('express');
+//import the database connection
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+//     //mongoose configurations
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+//use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+})
