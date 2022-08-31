0b59a1ddb2372f2d2e9c039ec0a44b9ce93b33d2
ThereWolf <bdunham484@gmail.com>
2022-08-31 14:50:23 -0500

    add reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,26 +1,62 @@
 //import the Schema constructor and the model function from mongoose
-const { Schema, model } = require('mongoose');
-//create the schema for the model using the Schema constructor and outline the fields
-const ThoughtSchema = new Schema({
-        thoughtText: {
+const { Schema, model, Types } = require('mongoose');
+//import the dateFormat() function
+const dateFormat = require('../utils/dateFormat');
+
+//create the schema for reactions
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
             type: String,
             required: true,
             min: 1,
             max: 280
         },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
         userName: {
             type: String,
             required: true
         },
-        reactions: []
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
     },
     {
         toJSON: {
+            getters: true
+        }
+    }
+);
+
+
+//create the schema for the model using the Schema constructor and outline the fields
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
             virtuals: true,
+            getters: true
         },
         id: false
     }
